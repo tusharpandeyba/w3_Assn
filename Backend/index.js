7bff466ff58fc9c15df91380b7ddc9606a27c839
tusharpandeyba <andchillupandey@gmail.com>
2025-01-17 13:24:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 //adding middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const fileupload = require("express-fileupload");
 app.use(fileupload({
